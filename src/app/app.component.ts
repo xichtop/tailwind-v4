e9e5664680cae7b8aa5699fa4611febad833909f
Xich Top <xichtop99@gmail.com>
2025-03-27 00:05:17 +0700

    feat: ✨ add theme color for material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [],
+  imports: [MatButtonModule, MatCardModule, MatSlideToggleModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
